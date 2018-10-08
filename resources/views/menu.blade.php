61b1ad734e6bd78afcc11a198d194ff22b1bcb26
lucho996 <luis.nova@virginiogomez.cl>
2018-10-08 16:30:47 -0300

    Menu

---
+++ resources/views/menu.blade.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Menu</title>
+    <link rel="stylesheet" href="css/botones_menu.css">
+    <link rel="stylesheet" href="css/css_icon.css">
+    <link rel="stylesheet" href="css/estilos.css">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+</head>
+
+</style>
+<body>
+
+    <header>
+        <div class="menu_bar">
+            <a href="#" class="bt_menu"><img src="imagenes/menu.png" class="imagen_menu">Menú</a>
+
+        </div>
+        <nav>
+            <ul class="mover">
+              <div class="botones">
+                <li><a href="#" > <img src="imagenes/home.png" class="imagen_icon"alt=""> Inicio</a></li>
+                <li><a href="#"><img src="imagenes/users.png" class="imagen_icon" alt=""> Nosotros</a></li>
+                <li><a href="#"><img src="imagenes/envelop.png" class="imagen_icon" alt=""> Contacto</a></li>
+                <li><a href="#"><img src="imagenes/intranet.png" class="imagen_icon" alt=""> Intranet</a></li>
+            </div>
+            </ul>
+        </nav>
+    </header>
+    <section  style="text-align:center;">
+        <p> to upload fonts for hosting or sharing. Using this feature, you can reference your fonts in HTML and easily change your icon selection on the fly, without having to update your CSS.</p>
+        <p> to upload fonts for hosting or sharing. Using this feature, you can reference your fonts in HTML and easily change your icon selection on the fly, without having to update your CSS.</p>
+        <p> to upload fonts for hosting or sharing. Using this feature, you can reference your fonts in HTML and easily change your icon selection on the fly, without having to update your CSS.</p>
+    </section>
+    <script src="http://code.jquery.com/jquery-latest.js"></script>
+    <script src="js/menu.js"></script>
+</body>
+</html>
