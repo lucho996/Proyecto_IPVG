53b17f682e9ae0c76363068d8c04ad665af76908
lucho996 <luis.nova@virginiogomez.cl>
2018-11-17 12:20:16 -0300

    luchin

---
+++ app/Detalle_Convenio.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * @property int $ID_DETALLE_CONVENIO
+ * @property int $ID_PRODUCTO
+ * @property int $ID_CONVENIO
+ * @property string $UNIDAD
+ * @property int $CANTIDAD
+ * @property int $VALOR_UNITARIO
+ * @property int $TOTAL
+ * @property Convenio $convenio
+ * @property Producto $producto
+ */
+class Detalle_Convenio extends Model
+{
+    /**
+     * The table associated with the model.
+     * 
+     * @var string
+     */
+    protected $table = 'detalle_convenio';
+
+    /**
+     * The primary key for the model.
+     * 
+     * @var string
+     */
+    protected $primaryKey = 'ID_DETALLE_CONVENIO';
+
+    /**
+     * @var array
+     */
+    protected $fillable = ['ID_PRODUCTO', 'ID_CONVENIO', 'UNIDAD', 'CANTIDAD', 'VALOR_UNITARIO', 'TOTAL'];
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function convenio()
+    {
+        return $this->belongsTo('App\Convenio', 'ID_CONVENIO', 'ID_CONVENIO');
+    }
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function producto()
+    {
+        return $this->belongsTo('App\Producto', 'ID_PRODUCTO', 'ID_PRODUCTO');
+    }
+}
